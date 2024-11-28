2839e5aeb99c8515d23dc3a9588e84012d501e2b
M.R. <118630069+Aeltestenrat@users.noreply.github.com>
2024-11-28 11:12:26 +0100

    Add files via upload

--- contact.js
+++ contact.js
@@ -33,6 +33,23 @@ function toggleMenu(sideMenu) {
     }}
 })
 
+function sendMail(event){﻿
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/xdkowbzo", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "./send_mail.html";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
 
 
 
