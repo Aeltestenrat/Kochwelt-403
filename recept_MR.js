b845daef21017a0a89d79118538759b3b4d98555
M.R. <118630069+Aeltestenrat@users.noreply.github.com>
2024-12-03 07:25:13 +0100

    Add files via upload

--- recept_MR.js
+++ recept_MR.js
@@ -1,3 +1,27 @@
 function toggleMenu() {
     document.getElementById("sideMenu").classList.toggle('sideMenuHide')
 }
+
+
+function equalIngredients2() {
+const portions2 = parseFloat(document.getElementById('input').value);
+
+const ingredientNeeded2 = document.querySelectorAll('.inputNeededGrey, .inputNeededWhite');
+
+ingredientNeeded2.forEach(input => {
+const originalValue = parseFloat(input.defaultValue);
+input.value = (originalValue * portions2).toFixed(1);
+});
+}
+
+function equalIngredients() {
+    const portions = parseFloat(document.getElementById('portionSelect').value); //vorher 'input
+
+    
+    const ingredientNeeded = document.querySelectorAll('.inputNeededGrey, .inputNeededWhite');
+
+ingredientNeeded.forEach(input => {
+const originalValue = parseFloat(input.defaultValue);
+input.value = (originalValue * portions).toFixed(1);
+});
+}
